6ea3db3253c6e72da93425a74e061ed14a5a77de
Serfelzxcv <kevin.carrillo.varas@gmail.com>
2024-05-27 13:16:53 -0500

    Actualizacion Responsive

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
+import react_icon from '../assets/img/react-icon.svg';
+import Angular_icon from '../assets/img/angular.svg';
+import flutter_icon from '../assets/img/flutter-icnon.svg';
+import node_icon from '../assets/img/Node.js_logo.svg';
+import sql_icon from '../assets/img/sql-icon.svg';
+import mongodb_icon from '../assets/img/mongodb-icon.svg';
+import docker_icon from '../assets/img/docker-icon.svg';
+import git_icon from '../assets/img/git-icon.svg';
 
 export const Skills = () => {
   const [active, setActive] = useState(2);
- 
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const items = [
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum hic iure enim, rem accusamus odit nemo aspernatur consequuntur vero in veniam fugiat, consectetur officiis voluptatum quidem libero. Sed, dignissimos exercitationem, animi a repellendus tempora recusandae qui consequatur, itaque deleniti nobis.",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptas ipsa similique magni adipisci accusamus consectetur fuga facilis doloremque dignissimos incidunt, reiciendis molestias dolore pariatur illum enim ipsum nostrum ab blanditiis? Voluptas, nostrum facere. Distinctio saepe facilis cumque! Voluptate, cumque.",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptas ipsa similique magni adipisci accusamus consectetur fuga facilis doloremque dignissimos incidunt, reiciendis molestias dolore pariatur illum enim ipsum nostrum ab blanditiis? Voluptas, nostrum facere. Distinctio saepe facilis cumque! Voluptate, cumque.",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptas ipsa similique magni adipisci accusamus consectetur fuga facilis doloremque dignissimos incidunt, reiciendis molestias dolore pariatur illum enim ipsum nostrum ab blanditiis? Voluptas, nostrum facere. Distinctio saepe facilis cumque! Voluptate, cumque.",
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptas ipsa similique magni adipisci accusamus consectetur fuga facilis doloremque dignissimos incidunt, reiciendis molestias dolore pariatur illum enim ipsum nostrum ab blanditiis? Voluptas, nostrum facere. Distinctio saepe facilis cumque! Voluptate, cumque.",
-    
+    {title:"React", img: react_icon, text:"React is a JavaScript library for building user interfaces."},
+    {title:"Angular", img: Angular_icon, text:"Angular is a JavaScript framework for building robust and dynamic web applications"},
+    {title:"Flutter", img: flutter_icon, text:"Flutter is a framework for building native cross-platform apps with a single codebase. "},
+    {title:"Node", img: node_icon, text:"Node.js is a runtime for executing JavaScript on the server side."},
+    {title:"SQL", img: sql_icon, text:"SQL is a language for managing and manipulating relational databases."},
+    {title:"MongoDB", img: mongodb_icon, text:"MongoDB is a NoSQL database that uses JSON documents to store data in collections."},
+    {title:"Docker", img: docker_icon, text:"It's a platform for creating, deploying, and running applications in isolated, portable containers."},
+    {title:"Git-GitHub", img: git_icon, text:"Git is a version control system for tracking changes in source code during software development."},
   ];
 
   const loadShow = () => {
@@ -18,35 +40,51 @@ export const Skills = () => {
       let className = "item";
       let style = {};
 
-      if (index === active) {
-        style = {
-          transform: `none`,
-          zIndex: 1,
-          filter: 'none',
-          opacity: 1
-        };
-      } else if (index > active ) {
-        const stt = index - active;
-        style = {
-          transform: `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`,
-          zIndex: -stt,
-          filter: 'blur(5px)',
-          opacity: stt > 2 ? 0 : 0.6
-        };
-      } else if (index < active) {
-        const stt = active - index;
-        style = {
-          transform: `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`,
-          zIndex: -stt,
-          filter: 'blur(5px)',
-          opacity: stt > 2 ? 0 : 0.6
-        };
+      if (!isMobile) {
+        if (index === active) {
+          style = {
+            transform: `none`,
+            zIndex: 1,
+            filter: 'none',
+            opacity: 1
+          };
+        } else if (index > active ) {
+          const stt = index - active;
+          style = {
+            transform: `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`,
+            zIndex: -stt,
+            filter: 'blur(5px)',
+            opacity: stt > 2 ? 0 : 0.6
+          };
+        } else if (index < active) {
+          const stt = active - index;
+          style = {
+            transform: `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`,
+            zIndex: -stt,
+            filter: 'blur(5px)',
+            opacity: stt > 2 ? 0 : 0.6
+          };
+        }
+      }
+
+      if (isMobile) {
+        if (index === active) {
+          style = {
+            transform: `none`,
+            zIndex: 1,
+            filter: 'none',
+            opacity: 1
+          }
+        } 
       }
 
       return (
         <div className={className} style={style} key={index}>
-          <h1>Slide {index + 1}</h1>
-          {item}
+          <h1>{item.title}</h1>
+          <img src={item.img} alt={`Slide ${index + 1}`} className="item-image" />
+          <div id='items-text'>
+            <p>{item.text}</p>
+          </div>
         </div>
       );
     });
@@ -59,6 +97,9 @@ export const Skills = () => {
   const prev = () => {
     setActive((prevActive) => (prevActive - 1 + items.length) % items.length);
   };
+  
+
+ 
 
   return (
     <section className="skill" id="skills">
@@ -70,9 +111,10 @@ export const Skills = () => {
               <p>Here are the skills I've acquired throughout my professional development:</p>
               <div className="slider">
                 {loadShow()}
-                <button id="prev" onClick={prev}>{'<'}</button>
-                <button id="next" onClick={next}>{'>'}</button>
+                
               </div>
+              <button id="prev" onClick={prev}>{'<'}</button>
+              <button id="next" onClick={next}>{'>'}</button>
             </div>
           </div>
         </div>
